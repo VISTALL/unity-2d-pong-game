82d933a19c447b75f31a3a6ad986600fbb646920
VISTALL <vistall.valeriy@gmail.com>
2015-07-29 11:02:51 +0300

    initial

---
+++ Assets/Standard Assets/SomeJSScript.js
@@ -0,0 +1,19 @@
+import System;
+
+class SomeJSScript extends MonoBehaviour
+{
+  var b : short = 1;
+
+  function Start() {
+    System.Console.WriteLine("test");
+    Console.WriteLine("Test");
+  }
+
+}
+
+class A extends MonoBehaviour {
+	function test() {
+		
+	}
+}
+
