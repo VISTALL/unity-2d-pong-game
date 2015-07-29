54136305d3fcc9886c38e6c4bbdee71c8c60ddd5
VISTALL <vistall.valeriy@gmail.com>
2015-07-29 19:28:10 +0300

    after add SS

---
+++ Assets/Standard Assets/SS.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class SS : MonoBehaviour {
+
+	public int FuckThis = 40;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
