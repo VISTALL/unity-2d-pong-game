1df07f702a9a820eed53cef55350f939dffa66eb
VISTALL <vistall.valeriy@gmail.com>
2016-11-20 17:14:58 +0300

    debug test

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -8,10 +8,27 @@ public class Ball : MonoBehaviour {
         // Initial Velocity
         GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
 
+        StartCoroutine(MyCoroutine(1));
     }
-    
+
+    IEnumerator MyCoroutine(int param) {
+        Debug.Log("init");
+        StartCoroutine(MyCoroutine2());
+        yield return null;
+
+    }
+
+    IEnumerator MyCoroutine2 () {
+
+        string check = "some value 2";
+        Debug.Log(check);
+
+        yield return null;
+
+    }
+
     float hitFactor(Vector2 ballPos, Vector2 racketPos,
-                    float racketHeight) {
+            float racketHeight) {
 
 
         // ascii art:
@@ -24,20 +41,19 @@ public class Ball : MonoBehaviour {
     }
 
 
-
     void OnCollisionEnter2D(Collision2D col) {
         // Note: 'col' holds the collision information. If the
         // Ball collided with a racket, then:
         //   col.gameObject is the racket
         //   col.transform.position is the racket's position
         //   col.collider is the racket's collider
-        
+
         // Hit the left Racket?
         if (col.gameObject.name == "RacketLeft") {
             // Calculate hit Factor
             float y = hitFactor(transform.position,
-                                col.transform.position,
-                                col.collider.bounds.size.y);
+                    col.transform.position,
+                    col.collider.bounds.size.y);
 
             // Calculate direction, make length=1 via .normalized
             Vector2 dir = new Vector2(1, y).normalized;
@@ -50,12 +66,12 @@ public class Ball : MonoBehaviour {
         if (col.gameObject.name == "RacketRight") {
             // Calculate hit Factor
             float y = hitFactor(transform.position,
-                                col.transform.position,
-                                col.collider.bounds.size.y);
+                    col.transform.position,
+                    col.collider.bounds.size.y);
 
             // Calculate direction, make length=1 via .normalized
             Vector2 dir = new Vector2(-1, y).normalized;
-            
+
             // Set Velocity with dir * speed
             GetComponent<Rigidbody2D>().velocity = dir * speed;
         }
